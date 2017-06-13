39696b42120fc9a04678f62ee6c26c33f5db4fb2
Arseniy Shestakov <me@arseniyshestakov.com>
2017-06-13 18:55:43 +0300

    Add encryption key in repository

--- CI/mxe/upload_package.sh
+++ CI/mxe/upload_package.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 cpack
 
-wget https://download.vcmi.eu/deploy_rsa_CHANGEME.enc
-touch /tmp/deploy_rsa_CHANGEME
-chmod 600 /tmp/deploy_rsa_CHANGEME
-openssl aes-256-cbc -K $encrypted_1d30f79f8582_key -iv $encrypted_1d30f79f8582_iv -in deploy_rsa_CHANGEME.enc -out /tmp/deploy_rsa_CHANGEME -d
+touch /tmp/deploy_rsa
+chmod 600 /tmp/deploy_rsa
+openssl aes-256-cbc -K $encrypted_1d30f79f8582_key -iv $encrypted_1d30f79f8582_iv -in $TRAVIS_BUILD_DIR/CI/deploy_rsa.enc -out /tmp/deploy_rsa -d
 eval "$(ssh-agent -s)"
-ssh-add /tmp/deploy_rsa_CHANGEME
+ssh-add /tmp/deploy_rsa
 
 sftp -r -o StrictHostKeyChecking=no travis@beholder.vcmi.eu <<< "put $VCMI_PACKAGE_FILE_NAME.exe /incoming/$VCMI_PACKAGE_FILE_NAME.exe"
